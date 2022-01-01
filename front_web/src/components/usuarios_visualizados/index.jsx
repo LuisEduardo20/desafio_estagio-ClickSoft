63ed81e73c85dc0942c7cfd346991c16e3a8c890
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-01-01 20:03:51 -0300

    Componente de lista de visualizados recentemente

--- front_web/src/components/usuarios_visualizados/index.jsx
+++ front_web/src/components/usuarios_visualizados/index.jsx
@@ -2,8 +2,10 @@ const ListaVisualizados = ({users, func}) => {
   return (
     <>
       <h3>Visualizados recentemente</h3>
-      { users.map( (user) => (
-          <p> {user} </p>
+      { users.map( (user, index) => (
+          <li key={`item${index}`}>
+            <button onClick={() => {func(user)}}> {user} </button>
+          </li>
         ))}
     </>
   );
