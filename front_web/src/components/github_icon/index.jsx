871d2699ae60cf2fb1b8cb42be9eceb5e5bfe073
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-01-15 12:17:00 -0300

    Componente de ícone do GitHub

---
+++ front_web/src/components/github_icon/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  } from '../styles';
+
+
+const GitHubIcon = () => {
+  return <img 
+          src="https://cdn.discordapp.com/attachments/914144800050143242/927737559210147890/icon_github.png" 
+          alt="Ícone do GitHub" 
+        />
+}
+
+export default GitHubIcon
